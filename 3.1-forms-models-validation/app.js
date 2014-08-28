62e6afcddcb8b97c87de39364dcb0542f04b0d8e
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-08-28 08:31:29 -0300

    indent code

--- 3.1-forms-models-validation/app.js
+++ 3.1-forms-models-validation/app.js
@@ -1,110 +1,119 @@
 (function() {
-  var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', []);
 
-  	app.controller('GalleryController', function(){
-		this.current=0;
-	    
-	    this.setCurrent = function(curr){
-			 this.current = curr || 0;
-	    };
-	});
+    app.controller('GalleryController', function(){
+        this.current=0;
 
-  app.controller('StoreController', function(){
-    this.products = gems;
-  });
+        this.setCurrent = function(curr){
+            this.current = curr || 0;
+        };
+    });
 
-  app.controller('TabController', function(){
-    this.tab = 1;
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
 
-    this.setTab = function(newValue){
-      this.tab = newValue;
-    };
+    app.controller('TabController', function(){
+        this.tab = 1;
 
-    this.isSet = function(tabName){
-      return this.tab === tabName;
-    };
-  });
+        //set active tab
+        this.setTab = function(newValue){
+            this.tab = newValue;
+        };
 
-  var gems = [
-    {
-      name: 'Azurite',
-      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-      shine: 8,
-      price: 110.50,
-      rarity: 7,
-      color: '#CCC',
-      faces: 14,
-      images: [
-        "http://placehold.it/380x150&text=Produto 00",
-        "http://placehold.it/380x150&text=Produto 01",
-        "http://placehold.it/380x150&text=Produto 02"
-      ],
-      reviews: [{
-        stars: 5,
-        body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
-      }]
-    },
-    {
-      name: 'Bloodstone',
-      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-      shine: 9,
-      price: 22.90,
-      rarity: 6,
-      color: '#EEE',
-      faces: 12,
-      images: [
-        "http://placehold.it/380x150&text=Produto 00",
-        "http://placehold.it/380x150&text=Produto 01",
-        "http://placehold.it/380x150&text=Produto 02"
-      ],
-      reviews: [{
-        stars: 3,
-        body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 4,
-        body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
-      }]
-    },
-    {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "http://placehold.it/380x150&text=Produto 00",
-        "http://placehold.it/380x150&text=Produto 01",
-        "http://placehold.it/380x150&text=Produto 02"
-      ],
-      reviews: [{
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }]
+        //verify if the current tab is active
+        this.isSet = function(tabName){
+            return this.tab === tabName;
+        };
+    });
+
+  var gems = [{
+        name: 'Azurite',
+        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+        shine: 8,
+        price: 110.50,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
+        images: [
+            "http://placehold.it/380x150&text=Produto 00",
+            "http://placehold.it/380x150&text=Produto 01",
+            "http://placehold.it/380x150&text=Produto 02"
+        ],
+        reviews: [
+            {
+                stars: 5,
+                body: "I love this gem!",
+                author: "joe@example.org",
+                createdOn: 1397490980837
+            },
+            {
+                stars: 1,
+                body: "This gem sucks.",
+                author: "tim@example.org",
+                createdOn: 1397490980837
+            }
+        ]
+    }, {
+        name: 'Bloodstone',
+        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+        shine: 9,
+        price: 22.90,
+        rarity: 6,
+        color: '#EEE',
+        faces: 12,
+        images: [
+            "http://placehold.it/380x150&text=Produto 00",
+            "http://placehold.it/380x150&text=Produto 01",
+            "http://placehold.it/380x150&text=Produto 02"
+        ],
+        reviews: [
+            {
+                stars: 3,
+                body: "I think this gem was just OK, could honestly use more shine, IMO.",
+                author: "JimmyDean@example.org",
+                createdOn: 1397490980837
+            },
+            {
+                stars: 4,
+                body: "Any gem with 12 faces is for me!",
+                author: "gemsRock@example.org",
+                createdOn: 1397490980837
+            }
+        ]
+    }, {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+            "http://placehold.it/380x150&text=Produto 00",
+            "http://placehold.it/380x150&text=Produto 01",
+            "http://placehold.it/380x150&text=Produto 02"
+        ],
+        reviews: [
+            {
+                stars: 1,
+                body: "This gem is WAY too expensive for its rarity value.",
+                author: "turtleguyy@example.org",
+                createdOn: 1397490980837
+            },
+            {
+                stars: 1,
+                body: "BBW: High Shine != High Quality.",
+                author: "LouisW407@example.org",
+                createdOn: 1397490980837
+            },
+            {
+                stars: 1,
+                body: "Don't waste your rubles!",
+                author: "nat@example.org",
+                createdOn: 1397490980837
+            }
+        ]
     }
   ];
 })();
